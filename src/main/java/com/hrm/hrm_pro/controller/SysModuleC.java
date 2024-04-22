882b9bdd489658633374dc6e521ca7c76cefe5c4
Choriyev Mirzohid <mirzohid.ch@gmail.com>
2024-04-22 17:43:22 +0500

    commit

--- src/main/java/com/hrm/hrm_pro/controller/SysModuleC.java
+++ src/main/java/com/hrm/hrm_pro/controller/SysModuleC.java
@@ -1,13 +1,46 @@
 package com.hrm.hrm_pro.controller;
 
+import com.hrm.hrm_pro.common.RedirectLogin;
+import com.hrm.hrm_pro.dto.EmpPositionDto;
+import com.hrm.hrm_pro.dto.SysModuleDto;
+import com.hrm.hrm_pro.model.system_user.UserCondition;
+import com.hrm.hrm_pro.repository.UserConditionRepo;
 import com.hrm.hrm_pro.service.SysModuleS;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class SysModuleC {
     final SysModuleS sysModuleS;
+    final UserConditionRepo userConditionRepo;
 
-    public SysModuleC(SysModuleS sysModuleS) {
+    public SysModuleC(SysModuleS sysModuleS, UserConditionRepo userConditionRepo) {
         this.sysModuleS = sysModuleS;
+        this.userConditionRepo = userConditionRepo;
+    }
+
+    @GetMapping("/sys-module")
+    public String getPage(
+            @RequestParam(value = "num", defaultValue = "0", required = false) int num,
+            @RequestParam(value = "size", defaultValue = "10", required = false) int size,
+            Model model,
+            HttpSession httpSession){
+        List<UserCondition> conditionList = userConditionRepo.findAll();
+        model.addAttribute("conditionList", conditionList);
+        model.addAttribute("list", sysModuleS.getAllSysModulePaging(num, size));
+        return RedirectLogin.redirectLogin("sys-module", httpSession);
+    }
+
+    @PostMapping("/sys-module/new")
+    public String save(@ModelAttribute("sysModuleDto") SysModuleDto sysModuleDto){
+        sysModuleS.save(sysModuleDto);
+        return "redirect:/sys-module";
     }
 }
--- src/main/java/com/hrm/hrm_pro/service/SysModuleS.java
+++ src/main/java/com/hrm/hrm_pro/service/SysModuleS.java
@@ -1,7 +1,14 @@
 package com.hrm.hrm_pro.service;
 
+import com.hrm.hrm_pro.dto.BankBlockDto;
+import com.hrm.hrm_pro.dto.PagingResponse;
 import com.hrm.hrm_pro.dto.SysModuleDto;
+import com.hrm.hrm_pro.model.system_emp.BankBlock;
+import com.hrm.hrm_pro.model.system_user.SysModule;
 import com.hrm.hrm_pro.repository.SysModuleRepo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,5 +23,41 @@ public class SysModuleS {
 
     public List<SysModuleDto> getAllSysModule(){
         return sysModuleRepo.getAllSysModule();
+    }
+
+    public PagingResponse getAllSysModulePaging(int pageNum, int pageSize) {
+        if (pageNum < 0){
+            pageNum = 0;
+        }
+        if (pageSize <= 0){
+            pageSize = 10;
+        }
+        Pageable pageable = PageRequest.of(pageNum, pageSize);
+        Page<SysModuleDto> sysModulePage = sysModuleRepo.getAllSysModulePaging(pageable);
+        List<?> sysModuleList = sysModulePage.stream().toList();
+
+        PagingResponse pagingResponse = new PagingResponse();
+        pagingResponse.setContent(sysModuleList);
+        pagingResponse.setPageNum(sysModulePage.getNumber());
+        pagingResponse.setPageSize(sysModulePage.getSize());
+        pagingResponse.setTotalPages(sysModulePage.getTotalPages());
+        pagingResponse.setTotalElements(sysModulePage.getTotalElements());
+        pagingResponse.setLast(sysModulePage.isLast());
+
+        return pagingResponse;
+    }
+
+    public void save(SysModuleDto sysModuleDto) {
+        SysModule sysModule = new SysModule();
+        sysModule.setCode(sysModuleDto.getCode());
+        sysModule.setName_uz(sysModuleDto.getName_uz());
+        sysModule.setName_ru(sysModuleDto.getName_ru());
+        sysModule.setName_en(sysModuleDto.getName_en());
+        sysModule.setCondition(sysModuleDto.getCondition());
+        sysModule.setPage_icon(sysModuleDto.getPage_icon());
+        sysModule.setPage_type(sysModuleDto.getPage_type());
+        sysModule.setPage_url(sysModuleDto.getPage_url());
+
+        sysModuleRepo.save(sysModule);
     }
 }
